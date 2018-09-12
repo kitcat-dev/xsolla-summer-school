83a1a6061b2e636c1c3785fdb29b4a9dbf482be0
albertmolodec <albert.abdu@gmail.com>
2018-09-12 15:38:57 +0500

    Readme conflict

--- src/App/components/Form/InputBlock.jsx
+++ src/App/components/Form/InputBlock.jsx
@@ -1,14 +1,14 @@
 import React, {Component } from 'react';
-import { Text } from 'informed';
+import { Text } from 'formik';
 
 import './PostForm.css';
 
 export default class InputBlock extends Component {
   render() {
-    const {colAmount, id, label} = this.props;
+    const {colAmount, id, label, field, validate, validateOnChange} = this.props;
 
     return (<div className={`col-${colAmount} input-block`}>
-        <Text field={`${id}`} type="text" id={`${id}`} placeholder=" "/>
+        <Text field={field} type="text" id={`${id}`} validateOnChange={validateOnChange} validate={validate} placeholder=" "/>
         <label htmlFor={`${id}`}>{label}</label>
     </div>);
   }
--- src/App/components/Form/PostForm.jsx
+++ src/App/components/Form/PostForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Form, Checkbox } from 'informed';
+import { Formik } from 'formik';
 
 import { UI } from '../../static/locale';
 import InputBlock from './InputBlock';
@@ -9,36 +9,11 @@ import './PostForm.css';
 export default class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.setFormApi = this.setFormApi.bind(this);
-
-    this.state = {};
   }
 
-  handleSubmit = () => {
-    event.preventDefault();    
-    const data = this.formApi.getState().values;
-
-    console.log(data);
-
-    // fetch('https://xsolla-ss-films-api.herokuapp.com/films', {
-    //   method: 'POST',
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(data),
-    // })
-    // .then(response => {
-    //   console.log('Response status: ' + response.status);
-    //   console.log('Response status text: ' + response.statusText);
-    //   console.log('Response url: ' + response.url);
-    // }, error => {
-    //   console.log('Error message: ' + error.message);
-    // })
-  }
-
-  setFormApi(formApi) {
-    this.formApi = formApi;
+  twoCharactersValidation = value => {
+    console.log('twoCharactersValidation');
+    return value.length < 2 ? `Field must be longer than 2 characters` : null;
   }
 
   render() {
@@ -46,85 +21,105 @@ export default class PostForm extends Component {
 
     return (
       <div className="form-wrap">
-        <Form action="" className="film-form" getApi={this.setFormApi}>
-          {({ formApi }) => (
+        <Form className="film-form" 
+              onChange={formState => console.log(formState.errors)}
+              onSubmit>
+          {({ formState }) => (
             <Fragment>
               {' '}
               <InputBlock
                 colAmount="1"
-                id="NameRU"
+                id="nameRU"
+                field="name.ru"
                 label={UI.postForm.ruFields.film[lang]}
+                validateOnChange
+                validate={this.twoCharactersValidation}
               />
-              <InputBlock
+              {}
+              {/* <InputBlock
                 colAmount={1}
-                id="NameEN"
+                id="nameEN"
+                field="name.en"
                 label={UI.postForm.enFields.film[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="directorNameRU"
+                field="directorName.ru"
                 label={UI.postForm.ruFields.directorName[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="directorNameEN"
+                field="directorName.en"
                 label={UI.postForm.enFields.directorName[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="famousPeopleRU"
+                field="famousPeople.ru"
                 label={UI.postForm.ruFields.famousPeople[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="famousPeopleEN"
+                field="famousPeople.en"
                 label={UI.postForm.enFields.famousPeople[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="descriptionRU"
+                field="description.ru"
                 label={UI.postForm.ruFields.description[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="descriptionEN"
+                field="description.en"
                 label={UI.postForm.enFields.description[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="whatILikeHereRU"
+                field="whatILikeHere.ru"
                 label={UI.postForm.ruFields.whatILikeHere[lang]}
               />
               <InputBlock
                 colAmount={1}
                 id="whatILikeHereEN"
+                field="whatILikeHere.en"
                 label={UI.postForm.enFields.whatILikeHere[lang]}
               />
               <InputBlock
                 colAmount={2}
                 id="releaseDate"
+                field="releaseDate"
                 label={UI.postForm.commonFields.releaseDate[lang]}
               />
               <InputBlock
                 colAmount={2}
                 id="watchingDate"
+                field="watchingDate"
                 label={UI.postForm.commonFields.watchingDate[lang]}
               />
               <InputBlock
                 colAmount={2}
                 id="imdbID"
+                field="imdbID"
                 label={UI.postForm.commonFields.imdbID[lang]}
               />
               <InputBlock
                 colAmount={2}
                 id="kpID"
+                field="kpID"
                 label={UI.postForm.commonFields.kpID[lang]}
               />
               <InputBlock
                 colAmount={2}
                 id="trailerYoutubeID"
+                field="trailerYoutubeID"
                 label={UI.postForm.commonFields.trailerYoutubeID[lang]}
-              />
+              /> */}
 
               <div className="col-2 input-checkbox">
                 <label htmlFor="checkbox-isFavourite">
@@ -133,10 +128,12 @@ export default class PostForm extends Component {
                 </label>
               </div>
               
-
-              <button type="submit" className="square-button" onClick={this.handleSubmit}>
+              <button type="submit" className="square-button">
                 {UI.postForm.commonFields.addFilmButton[lang]}
               </button>
+              {/* <button type="submit" className="square-button" onClick={this.handleSubmit}>
+                {UI.postForm.commonFields.addFilmButton[lang]}
+              </button> */}
             </Fragment>
           )}
         </Form>
@@ -144,3 +141,24 @@ export default class PostForm extends Component {
     );
   }
 }
+
+
+  // handleSubmit = () => {
+  //   // event.preventDefault();    
+  //   const data = this.formApi.getState().values;
+
+  //   // fetch('https://xsolla-ss-films-api.herokuapp.com/films', {
+  //   //   method: 'POST',
+  //   //   headers: {
+  //   //     "Content-Type": "application/json"
+  //   //   },
+  //   //   body: JSON.stringify(data),
+  //   // })
+  //   // .then(response => {
+  //   //   console.log('Response status: ' + response.status);
+  //   //   console.log('Response status text: ' + response.statusText);
+  //   //   console.log('Response url: ' + response.url);
+  //   // }, error => {
+  //   //   console.log('Error message: ' + error.message);
+  //   // })
+  // }
